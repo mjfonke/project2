a6b51a1ad54bee7f2073e4a3ee14fc40c7cf265a
Maurice Webb <mwebb.mw@gmail.com>
2019-10-10 17:53:51 -0400

    Working on awesome app

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: process.env.SEQUELIZE_USER,
     password: process.env.SEQUELIZE_PASSWORD,
-    database: 'project2_dev',
+    database: 'moods_db',
     details: {
       host: process.env.SEQUELIZE_HOST,
       port: 3306,
@@ -12,7 +12,7 @@ module.exports = {
   test: {
     username: process.env.TU,
     password: process.env.TP || null,
-    database: 'project2_test',
+    database: 'moods_db',
     details: {
       host: 'localhost',
       port: 3306,
