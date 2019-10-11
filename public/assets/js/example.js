7f15117861552aa25b5424f68d3abd66dfcbe0b6
Miran Jang <miranjang9507@gmail.com>
2019-10-11 13:56:53 -0400

    functioning slider on example.handlebars

--- public/assets/js/example.js
+++ public/assets/js/example.js
@@ -1,97 +1,45 @@
-// Get references to page elements
-const $exampleText = $('#example-text');
-const $exampleDescription = $('#example-description');
-const $submitBtn = $('#submit');
-const $exampleList = $('#example-list');
-
-// The API object contains methods for each kind of request we'll make
-const API = {
-  saveExample: function (example) {
-    return $.ajax({
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      type: 'POST',
-      url: 'api/examples',
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function () {
-    return $.ajax({
-      url: 'api/examples',
-      type: 'GET'
-    });
-  },
-  deleteExample: function (id) {
-    return $.ajax({
-      url: 'api/examples/' + id,
-      type: 'DELETE'
-    });
-  }
-};
-
-// refreshExamples gets new examples from the db and repopulates the list
-const refreshExamples = function () {
-  API.getExamples().then(function (data) {
-    const $examples = data.map(function (example) {
-      const $a = $('<a>')
-        .text(example.text)
-        .attr('href', '/example/' + example.id);
-
-      const $li = $('<li>')
-        .attr({
-          class: 'list-group-item',
-          'data-id': example.id
-        })
-        .append($a);
-
-      const $button = $('<button>')
-        .addClass('btn btn-danger float-right delete')
-        .text('ｘ');
-
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
+$(document).ready(function () {
+  $('#mySlider1').slider({
+    range: 'max',
+    min: 1,
+    max: 100,
+    value: 0,
+    slide: function (event, ui) {
+      $('#q1').val(ui.value);
+    }
   });
-};
+  $('#q1').val($('#mySlider1').slider('value'));
 
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-const handleFormSubmit = function (event) {
-  event.preventDefault();
-
-  const example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
-    alert('You must enter an example text and description!');
-    return;
-  }
-
-  API.saveExample(example).then(function () {
-    refreshExamples();
+  $('#mySlider2').slider({
+    range: 'max',
+    min: 1,
+    max: 100,
+    value: 0,
+    slide: function (event, ui) {
+      $('#q2').val(ui.value);
+    }
   });
+  $('#q2').val($('#mySlider2').slider('value'));
 
-  $exampleText.val('');
-  $exampleDescription.val('');
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-const handleDeleteBtnClick = function () {
-  const idToDelete = $(this).parent().attr('data-id');
-
-  API.deleteExample(idToDelete).then(function () {
-    refreshExamples();
+  $('#mySlider3').slider({
+    range: 'max',
+    min: 1,
+    max: 100,
+    value: 0,
+    slide: function (event, ui) {
+      $('#q3').val(ui.value);
+    }
   });
-};
+  $('#q3').val($('#mySlider3').slider('value'));
 
-// Add event listeners to the submit and delete buttons
-$submitBtn.on('click', handleFormSubmit);
-$exampleList.on('click', '.delete', handleDeleteBtnClick);
+  $('#mySlider4').slider({
+    range: 'max',
+    min: 1,
+    max: 100,
+    value: 0,
+    slide: function (event, ui) {
+      $('#q4').val(ui.value);
+    }
+  });
+  $('#q4').val($('#mySlider4').slider('value'));
+}); 
