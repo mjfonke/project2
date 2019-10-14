aca148fabd08aa7c5bab5a74374cb5dc2fc13761
Ryan Kooy <48595828+ryankooy@users.noreply.github.com>
2019-10-14 15:06:28 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -12,7 +12,7 @@ module.exports = {
   test: {
     username: process.env.TU,
     password: process.env.TP || null,
-    database: 'moods_db',
+    database: 'project2_test',
     details: {
       host: 'localhost',
       port: 3306,
