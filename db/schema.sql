166a1074b095f03d88c78652e3c07456af1ac0af
Ryan Kooy <rwkooy@gmail.com>
2019-10-15 17:28:46 -0400

    navbar adjustments - user appears to be logged out when on /example page

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,17 @@
-create database project2_dev;
+-- create database project2_dev;
+
+use moods_db;
+
+create table moods (
+  id int not null auto_increment,
+  excited int(10) null,
+  tired int(10) null,
+  happy int(10) null,
+  upset int(10) null,
+  primary key (id)
+);
+
+insert into moods (excited, tired, happy, upset)
+values (5, 7, 4, 2);
+
+select * from moods;
