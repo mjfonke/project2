9698c069623af9fce2711cdfcbc15e5df4343a9a
Ryan Kooy <48595828+ryankooy@users.noreply.github.com>
2019-10-15 21:22:56 -0400

    Update example.js

--- models/example.js
+++ models/example.js
@@ -3,20 +3,5 @@ module.exports = function (sequelize, DataTypes) {
     text: DataTypes.STRING,
     description: DataTypes.TEXT
   });
-<<<<<<< HEAD
   return Example;
-=======
-
-  Mood.associate = function (models) {
-    // We're saying that a Post should belong to an Author
-    // A Post can't be created without an Author due to the foreign key constraint
-    Mood.belongsTo(models.User, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
-  };
-
-  return Mood;
->>>>>>> 04d36789d6661789ef87e56afdbb455b6b5044dc
 };
