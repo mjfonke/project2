22ffbe367a585d5607f990e30b4bc5d020ada4cf
Ryan Kooy <48595828+ryankooy@users.noreply.github.com>
2019-10-17 20:53:51 -0400

    Update seed.js

--- db/seed.js
+++ db/seed.js
@@ -2,20 +2,15 @@ module.exports = (db) => {
   // db.User.create({
   //   firstName: 'Bill',
   //   lastName: 'Bones',
-  //   email: 'b@g.co',
+  //   email: 'bb@g.co',
   //   password: process.env.ADMIN_USER_PWD,
   //   isAdmin: true
   // });
   // db.User.create({
   //   firstName: 'Marge',
   //   lastName: 'Meddler',
-  //   email: 'm@j.co',
+  //   email: 'mm@j.co',
   //   password: process.env.USER_PWD,
   //   isAdmin: false
   // });
-  db.Mood.create({
-    alert: 3,
-    happy: 7,
-    relaxed: 9
-  });
 };
