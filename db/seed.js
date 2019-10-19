c45aa0637e25c7943b7d4de6f85ed511c41f5575
Ryan Kooy <rwkooy@gmail.com>
2019-10-18 22:45:51 -0400

    original toggle buttons back

--- db/seed.js
+++ db/seed.js
@@ -2,57 +2,67 @@ module.exports = (db) => {
   db.User.create({
     firstName: 'Bill',
     lastName: 'Bones',
-    email: 'b@g.co',
+    email: 'bb@g.co',
     password: process.env.ADMIN_USER_PWD,
     isAdmin: true
   });
   db.User.create({
     firstName: 'Marge',
     lastName: 'Meddler',
-    email: '123',
+    email: 'mm@g.co',
     password: process.env.USER_PWD,
     isAdmin: false
   });
-  // db.Moods.create({
-  //   id: 0,
-  //   alert: 4,
-  //   happy: 7,
-  //   relaxed: 3,
-  //   eaten: 1,
-  //   slept: 0,
-  //   showered: 1,
-  //   worked: 1,
-  //   exercised: 0,
-  //   createdAt: NOW(),
-  //   updatedAt: NOW(),
-  //   UserId: 4
-  // });
-  // db.Moods.create({
-  //   id: 1,
-  //   alert: 8,
-  //   happy: 6,
-  //   relaxed: 6,
-  //   eaten: 1,
-  //   slept: 1,
-  //   showered: 0,
-  //   worked: 0,
-  //   exercised: 0,
-  //   createdAt: NOW(),
-  //   updatedAt: NOW(),
-  //   UserId: 4
-  // });
-  // db.Moods.create({
-  //   id: 1,
-  //   alert: 3,
-  //   happy: 6,
-  //   relaxed: 4,
-  //   eaten: 0,
-  //   slept: 0,
-  //   showered: 0,
-  //   worked: 1,
-  //   exercised: 0,
-  //   createdAt: NOW(),
-  //   updatedAt: NOW(),
-  //   UserId: 4
-  // });
+  db.Mood.create({
+    alert: 4,
+    happy: 7,
+    relaxed: 3,
+    eaten: 1,
+    slept: 0,
+    showered: 1,
+    worked: 1,
+    exercised: 0,
+    createdAt: '2019-10-18 16:48:21',
+    updatedAt: '2019-10-18 16:48:21',
+    UserId: 10
+  });
+  db.Mood.create({
+    alert: 3,
+    happy: 2,
+    relaxed: 4,
+    eaten: 0,
+    slept: 0,
+    showered: 0,
+    worked: 1,
+    exercised: 0,
+    createdAt: '2019-10-14 18:00:00',
+    updatedAt: '2019-10-14 18:00:00',
+    UserId: 11
+  });
+  db.Mood.create({
+    alert: 7,
+    happy: 9,
+    relaxed: 7,
+    eaten: 1,
+    slept: 1,
+    showered: 0,
+    worked: 1,
+    exercised: 1,
+    createdAt: '2019-10-13 13:00:00',
+    updatedAt: '2019-10-13 13:00:00',
+    UserId: 12
+  });
+  db.Mood.create({
+    alert: 7,
+    happy: 9,
+    relaxed: 7,
+    eaten: 1,
+    slept: 1,
+    showered: 0,
+    worked: 1,
+    exercised: 1,
+    createdAt: '2019-10-13 13:00:00',
+    updatedAt: '2019-10-13 13:00:00',
+    UserId: 12
+  });
 };
